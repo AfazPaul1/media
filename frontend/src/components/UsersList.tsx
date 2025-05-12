1fa6b1f664e1559f6bb112429199789f00c8ae59
AfazPaul1 <afazpaul@gmail.com>
2025-05-12 17:20:32 +0530

    implemented thunks to fetch users and added extrareducers for usersslice to update state according to response loaded components conditionally : create a folder thunks in store name the file apptly like what does this thunk do - fetchusers and import createAsyncThunk and axios. createthunk takes 2 arguments the basetype string and the async function. the basetype is used to create the 3 action types pending fulfilled rejected. we use axios.get(url) and await the response and return its data property and export the thunk. added extrareducers where the type arg is given by thunk.pending which corresponds to users/fetch/pending on which we set isloading state to true. similarly for rem 2 action types. then we import the thunk into the component and dispatch is on first render using useeffect. when we first render when request made it first dispatches the pending action  - ui update followed by when response recieved dispatches fulfilled action

--- frontend/src/components/UsersList.tsx
+++ frontend/src/components/UsersList.tsx
@@ -1,6 +1,24 @@
+import { useDispatch, useSelector} from "react-redux"
+import { useEffect } from "react"
+import {fetchUsers} from '../store'
 function UsersList() {
+    const dispatch = useDispatch()
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users
+    })
+    
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if (isLoading) {
+        return 'loading'
+    }
+    if (error) {
+        return error.name
+    }
     return (
-        <div>hiu</div>
+        <div>{data.length}</div>
     )
 }
 
