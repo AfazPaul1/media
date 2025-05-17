903f6b66f450130e337f6ef0816b64a57d5e687b
AfazPaul1 <afazpaul@gmail.com>
2025-05-17 16:05:33 +0530

    refactor(users): modularize expandable userListItem UI into ExpandablePanel component for reuse and cleaner structure. moved all elements depended on the item's expanded state into that component

---
+++ frontend/src/components/ExpandablePanel.tsx
@@ -0,0 +1,39 @@
+import { Grid,Paper,IconButton } from "@mui/material"
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import { useState } from "react";
+function ExpandablePanel({header, children}) {
+
+    const [isExpanded, setIsExpanded] = useState()
+    const handleExpand = () => {
+        setIsExpanded(!isExpanded)
+    }
+
+    return (
+        <>
+        <Grid container justifyContent="center">
+            <Grid size={6}>
+                <Paper elevation={3} sx={{margin:1}}>
+                <Grid  container  justifyContent="center">
+                    {header}
+                    <Grid size={2}>
+                        <IconButton onClick={handleExpand}>
+                            {isExpanded? <ArrowDropDownIcon fontSize="large"/> : <ArrowLeftIcon fontSize="large"/>}
+                        </IconButton>
+                    </Grid>
+                </Grid>
+                </Paper>
+            </Grid>
+        </Grid>
+        {
+            isExpanded && 
+            <Grid container justifyContent="center">
+                <Grid size={6} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    {children}
+                </Grid>
+            </Grid>
+        }
+        </>
+    )
+}
+export default ExpandablePanel
--- frontend/src/components/UsersListItem.tsx
+++ frontend/src/components/UsersListItem.tsx
@@ -3,6 +3,7 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
 import {deleteUser} from '../store'
 import { Grid, Typography, IconButton, Paper } from '@mui/material';
+import ExpandablePanel from "./ExpandablePanel";
 
 function UsersListItem({user}) {
 
@@ -11,29 +12,23 @@ function UsersListItem({user}) {
         doDeleteUser(id)
     }
 
-    return (
-        <Grid container justifyContent="center">
-            <Grid size={6}>
-                <Paper elevation={3} sx={{margin:1}}>
-                <Grid  container  justifyContent="center">
-                    <Grid size={2}>
-                        <IconButton loading={isDeletingUser} onClick={() => handleDeleteUser(user.id)}>
-                            <PersonRemoveIcon fontSize="large"/>
-                        </IconButton>
+    const header = 
+            <>
+                <Grid size={2}>
+                    <IconButton loading={isDeletingUser} onClick={() => handleDeleteUser(user.id)}>
+                        <PersonRemoveIcon fontSize="large"/>
+                    </IconButton>
                         
-                    </Grid>
-                    <Grid size={8} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Typography align="center">{user.name}</Typography>
-                    </Grid>
-                    <Grid size={2}>
-                        <IconButton>
-                            <ArrowDropDownIcon fontSize="large"/>
-                        </IconButton>
-                    </Grid>
                 </Grid>
-                </Paper>
-            </Grid>
-        </Grid>
+                <Grid size={8} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography align="center">{user.name}</Typography>
+                </Grid>
+            </>
+
+    return (        
+                    <ExpandablePanel header={header}>
+                        Expanded
+                    </ExpandablePanel>               
     )
 }
 
