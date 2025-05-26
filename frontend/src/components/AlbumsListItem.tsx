db71b97295b115666a973e32525a00a5b93e5a48
AfazPaul1 <afazpaul@gmail.com>
2025-05-26 17:53:55 +0530

    refactor: use ConfirmDialog for album deletion confirmation body: create state to track open state. on album delete click we open dialog if photos exists or call hook if no photos exist. onClose prop takes a callback inide which we setstate. onconfirm function inside which we call the hook's trigger function

--- frontend/src/components/AlbumsListItem.tsx
+++ frontend/src/components/AlbumsListItem.tsx
@@ -3,11 +3,23 @@ import { Typography, IconButton } from "@mui/material"
 import RemoveIcon from '@mui/icons-material/Remove';
 import { useDeleteAlbumsMutation } from "../store";
 import PhotosList from "./PhotosList";
+import ConfirmDialog from "./ConfirmDialog";
+import { useState } from "react";
+import { useFetchPhotosQuery } from "../store";
 function AlbumsListItem({album}) {
     const [deleteAlbum, {isLoading: isDeletingAlbum}] = useDeleteAlbumsMutation()
+    const {data: photos} = useFetchPhotosQuery(album)
+   
     const handleDeleteAlbum = (album) => {
-        deleteAlbum(album)
+        console.log(photos.length, album);
+        if(photos.length){
+            setIsOpen(true)
+        }
+        else{
+            deleteAlbum(album)
+        }   
     }
+    const [isOpen, setIsOpen] = useState(false)
     const header =
                     <>
                     <IconButton loading={isDeletingAlbum} onClick={() => handleDeleteAlbum(album)}>
@@ -17,9 +29,20 @@ function AlbumsListItem({album}) {
                     </> 
 
         
-    return <ExpandablePanel header={header}>
-        <PhotosList album={album}/>
-    </ExpandablePanel>
+    return (
+    <>
+        <ExpandablePanel header={header}>
+            <PhotosList album={album}/>
+        </ExpandablePanel>
+        <ConfirmDialog 
+        open={isOpen} 
+        onClose={() => setIsOpen(false)}
+        onConfirm={() => deleteAlbum(album)}
+        title="Delete Album?"
+        description="Deleting this album will also delete all associated photos. Are you sure you want to continue?"
+        />
+    </>
+    )
 }
 
 export default AlbumsListItem
